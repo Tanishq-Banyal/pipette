ccbc5cbb559057305c5e5bddcb15ca73d9cb6ecf
Tanishq-Banyal <68330937+Tanishq-Banyal@users.noreply.github.com>
2021-12-02 22:07:06 +0530

    Update pipe.hpp

--- pipe.hpp
+++ pipe.hpp
@@ -8,14 +8,17 @@
 #include <cstddef>
 #include <fcntl.h>
 
-class Pipe
+namespace pipette
+{
+
+class pipe
 {
 private:
 	FILE* fp = NULL;
 public:
-   ~Pipe() { Close(); }
+   ~pipe() { Close(); }
 
-	int Open(const char* cmdline, bool write = false)
+	int open(const char* cmdline, bool write = false)
 	{
 		fp = popen(cmdline, write ? "w" : "r");	// rb & wb are not working for some reason
 		if (!fp) return -1; // return WEXITSTATUS(pclose(fp));
@@ -23,7 +26,7 @@ public:
 		return 0;
 	}
 
-	int Close()
+	int close()
 	{
 		if (fp)
 		{
@@ -33,13 +36,15 @@ public:
 		return 0;
 	}
 
-	size_t Read(uint8_t* buffer, size_t size)
+	size_t read(uint8_t* buffer, size_t size)
 	{
         return fread(buffer, 1, size, fp);
     }
 
-	size_t Write(uint8_t* buffer, size_t size)
+	size_t write(uint8_t* buffer, size_t size)
 	{
 		return fwrite(buffer, 1, size, fp);
 	}
 };
+
+}
