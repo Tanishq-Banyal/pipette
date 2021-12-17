7ed09f5aee125c25b20c334197fb6bda9c137000
sandeep <sandeepqmar05@gmail.com>
2021-12-17 16:14:01 +0530

    Added bi directional communication between processes

--- pipe/pipe.hpp
+++ pipe/pipe.hpp
@@ -1,23 +1,35 @@
-#include <cstdio>
 #include <cerrno>
-#include <cstdint>
-#include <cstdlib>
 #include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
 #include <fcntl.h>
 
-namespace pipette
-{
+namespace pipette {
 
-class pipe
-{
+class pipe {
 private:
-	FILE* fp = NULL;
+  FILE *fp = NULL;
+
 public:
-	int open(const char* cmdline, bool write=false);
-	size_t read(uint8_t* buffer, size_t size) const;
-	size_t write(uint8_t* buffer, size_t size) const;
-	int close();
-	~pipe() {close();}
+  int open(const char *cmdline, bool write = false);
+  size_t read(uint8_t *buffer, size_t size) const;
+  size_t write(uint8_t *buffer, size_t size) const;
+  int close();
+  ~pipe() { close(); }
 };
 
-}
+class pipe_bi {
+private:
+  int parent_pipes[2];
+  int child_pipes[2];
+  int pid;
+
+public:
+  int open(const char *cmdline);
+  size_t read(void* buffer, size_t size) const;
+  size_t write(void* buffer, size_t size) const;
+  int close();
+  ~pipe_bi() { close(); }
+};
+} // namespace pipette
